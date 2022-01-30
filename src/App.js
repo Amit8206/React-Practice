5c229d53752d1bdc81920a006be740486929e6f3
Amit8206 <amitkumarsahoo572@gmail.com>
2022-01-30 21:13:40 +0530

    Enhancement in Dark Mode Adds in Application And Inprove Some More Features

--- src/App.js
+++ src/App.js
@@ -2,12 +2,64 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alert from './components/Alert';
+
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const ads = () => {
+    setTimeout(() => {
+      setInterval(() => {
+        document.title = 'TextUtils Is a Amazing'
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install TextUtils Now'
+      }, 1500);
+    }, 2000);
+  }
+
+  const toogleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#101048'  // #042743(Ocene Blue)  #101048(light blue)
+      showAlert("Dark Mode Enabled", "success")
+      document.title = 'TextUtils | Dark Mode'
+      // ads()
+
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light Mode Enabled", "success")
+      document.title = 'TextUtils | Light Mode'
+      // ads()
+    }
+  }
+
+
+
+
+
   return (
+
     <>
-      <Navbar title="TextUtils"/>
-      <TextForm heading="Enter the text to Analyze" />
+      {ads()}
+      <Navbar title="TextUtils" mode={mode} toogleMode={toogleMode} />
+      <Alert alert={alert} />
+      <TextForm showAlert={showAlert} heading="Enter the text to Analyze" mode={mode} />
       {/* <About /> */}
     </>
   );
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -8,30 +8,36 @@ export default function TextForm(props) {
         // console.log("HandleUpClick Botton Clicked " + text)
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Text Converted to Upper Case", "success")
     }
     const handleLoClick = () => {
         // console.log("HandleUpClick Botton Clicked " + text)
         let newText = text.toLowerCase();
         setText(newText)
-    }   
+        props.showAlert("Text Converted to Lower Case", "success")
+    }
     const handleClearText = () => {
         // console.log("HandleUpClick Botton Clicked " + text)
         let newText = ""
         setText(newText)
+        props.showAlert("Text Clear Successfuly", "success")
     }
-    const handleCopyText = () =>{
+    const handleCopyText = () => {
         let text = document.getElementById('myText')
         text.select()
         navigator.clipboard.writeText(text.value)
+        props.showAlert("Text Copied Successfully", "success")
 
     }
     const convertToTitlecase = () => {
         setText(text[0].toUpperCase() + text.substring(1).toLowerCase());
+        props.showAlert("Text Converted 1st Char Title Case", "success")
     }
     const removeExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
-         setText(newText.join(" "))
-     }
+        setText(newText.join(" "))
+        props.showAlert("Extra Spacess Removed Successfully", "success")
+    }
 
 
 
@@ -46,30 +52,32 @@ export default function TextForm(props) {
 
     return (
         <>
-            <div className="container my-3">
+                <div className="container my-5">
 
-                <h1 className='heading text-center text-secondary'>{props.heading}</h1>
+                    <h1 className='heading text-center' style={{color: props.mode==='dark'?'white':'gray'}}>{props.heading}</h1>
 
-                <div className="mb-3 my-3">
-                    <textarea className="form-control" placeholder='Enter Text Here' value={text} onChange={handleOnChange} id="myText" rows="10"></textarea>
+                    <div className="mb-3 my-3">
+                        <textarea className="form-control" 
+                        style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'black'}} 
+                        placeholder='Enter Text Here' value={text} onChange={handleOnChange} id="myText" rows="10"></textarea>
+                    </div>
+                    <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                    <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+                    <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
+                    <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy Text</button>
+                    <button className="btn btn-primary mx-2" onClick={removeExtraSpaces}>Remove Spacess</button>
+                    <button className="btn btn-primary mx-2" onClick={convertToTitlecase}>1st Cap Char</button>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy Text</button>
-                <button className="btn btn-primary mx-2" onClick={removeExtraSpaces}>Remove Spacess</button>
-                <button className="btn btn-primary mx-2" onClick={convertToTitlecase}>1st Cap Char</button>
-            </div>
 
-            <div className="container my-5">
-                <h1 className='summary text-center text-primary'>Your Text Summary</h1>
-                <p className='result text-center my-3'><b>{text.split(" ").length}</b> Words And <b>{text.length}</b> characters</p>
-                <h5 className='result text-center text-success my-3'>It Can Be Read in <b>{ 0.008 * text.split(" ").length }</b> Minutes... </h5>
+                <div className="container my-5">
+                    <h1 className='summary text-center text-primary'>Your Text Summary</h1>
+                    <p style={{color: props.mode==='dark'?'white':'black'}} className='result text-center my-3'><b>{text.split(" ").length}</b> Words And <b>{text.length}</b> characters</p>
+                    <h5 className='result text-center text-success my-3'>It Can Be Read in <b>{0.008 * text.split(" ").length}</b> Minutes... </h5>
 
-                <h3 className='preview my-3'>Preview</h3>
-                <p className='preview-text my-2'>{text}</p>
-            </div>
-
+                    <h3 className='preview my-3 text-secondary'>Preview</h3>
+                    <p className='preview-text my-2' style={{color: props.mode==='dark'?'white':'black'}}>
+                        {text.length>0 ? text : 'Enter Some Text in the Above box To Preview It..'}</p>
+                </div>
         </>
     )
 }
