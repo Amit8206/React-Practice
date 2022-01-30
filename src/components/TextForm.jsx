b8d193b329575e80b2bed53c106b451dfce36d86
Amit8206 <amitkumarsahoo572@gmail.com>
2022-01-30 12:50:41 +0530

    Adding Dark Mode And Add Some More features

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -13,26 +13,52 @@ export default function TextForm(props) {
         // console.log("HandleUpClick Botton Clicked " + text)
         let newText = text.toLowerCase();
         setText(newText)
+    }   
+    const handleClearText = () => {
+        // console.log("HandleUpClick Botton Clicked " + text)
+        let newText = ""
+        setText(newText)
     }
+    const handleCopyText = () =>{
+        let text = document.getElementById('myText')
+        text.select()
+        navigator.clipboard.writeText(text.value)
+
+    }
+    const convertToTitlecase = () => {
+        setText(text[0].toUpperCase() + text.substring(1).toLowerCase());
+    }
+    const removeExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+         setText(newText.join(" "))
+     }
+
+
+
+
     const handleOnChange = (event) => {
         // console.log("On Change")
         setText(event.target.value)
     }
 
     // Declare a new state variable, which we'll call "count"
-    const [text, setText] = useState('Enter Text Here');
+    const [text, setText] = useState('');
 
     return (
         <>
-            <div className="container my-5">
+            <div className="container my-3">
 
                 <h1 className='heading text-center text-secondary'>{props.heading}</h1>
 
                 <div className="mb-3 my-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+                    <textarea className="form-control" placeholder='Enter Text Here' value={text} onChange={handleOnChange} id="myText" rows="10"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={removeExtraSpaces}>Remove Spacess</button>
+                <button className="btn btn-primary mx-2" onClick={convertToTitlecase}>1st Cap Char</button>
             </div>
 
             <div className="container my-5">
@@ -41,7 +67,7 @@ export default function TextForm(props) {
                 <h5 className='result text-center text-success my-3'>It Can Be Read in <b>{ 0.008 * text.split(" ").length }</b> Minutes... </h5>
 
                 <h3 className='preview my-3'>Preview</h3>
-                <p className='pri-text my-2'>{text}</p>
+                <p className='preview-text my-2'>{text}</p>
             </div>
 
         </>
